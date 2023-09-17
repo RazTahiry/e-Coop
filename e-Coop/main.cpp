1eea30fed4523f00766753d4a973d238d15fbf83
RazTahiry <tahiryrazanamara01@gmail.com>
2023-09-17 08:47:22 +0300

    windows added and design

--- e-Coop/main.cpp
+++ e-Coop/main.cpp
@@ -1,11 +1,13 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QtWidgets>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    QWidget *centralWidget = new QWidget(&w);
     w.setWindowTitle("e-Coop");
     w.show();
     return a.exec();
